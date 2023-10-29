581963068969a814a70fb1f22794f092d70a6833
Niko Partanen <nikotapiopartanen@gmail.com>
2023-10-29 22:59:44 +0200

    updates

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python3
 
-import flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 import os
+import xml.etree.ElementTree as ET
 
-application = flask.Flask(__name__)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+Bootstrap(app)
 
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
-@application.route('/')
-def hello():
-    name = flask.request.args.get("name", "World")
+@app.route('/', methods=['GET'])
+def upload():
+    return render_template('upload.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        try:
+            statistics = get_eaf_statistics(file_path)
+            return render_template('results.html', statistics=statistics)
+        except ET.ParseError:
+            error = "The uploaded file is not a valid .eaf XML file."
+            return render_template('upload.html', error=error)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'eaf'
+
+def get_eaf_statistics(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    annotations = root.findall('.//ANNOTATION')
+    num_annotations = len(annotations)
     return {
-        'msg': 'Hello, {}!'.format(flask.escape(name))
+        'num_annotations': num_annotations,
     }
 
-
-if __name__ == "__main__":
-    application.run()
+if __name__ == '__main__':
+    app.run(debug=True)
